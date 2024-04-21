733516e797daed6d888768814684c21a8ebac1dc
Alain Ramon Burkhard <114851831+AlainRBurkhard@users.noreply.github.com>
2024-04-21 19:44:13 +0200

    Update Home.py
    update script. first attempt

--- Home.py
+++ Home.py
@@ -13,6 +13,70 @@ import streamlit as st
 st.set_page_config( page_title= 'e-Commerce Dashboard', page_icon = '📱', layout = 'wide')
 
 
+daf=pd.read_csv('Data/merged_products_20240416.csv.csv')
+df = daf.copy()
+
+df.rename(columns={'rating_100': 'score'}, inplace=True)
+df_valid = df[df['delivery_time'].notna()]
+
+###########################################################################################################
+# Our Top
+
+df_recommended = df_recommended.sort_values(
+    by=['score', 'price', 'delivery_time', 'reviews_count', 'memory_GB', 'camera_MP'],
+    ascending=[False, True, True, False, False, False])
+
+df_recommended.reset_index(drop=True, inplace=True)
+
+df_recommended.drop(columns='id', inplace=True)
+
+df_recommended.reset_index(drop=True, inplace=True)
+df_recommended.index += 1
+df_recommended['id'] = df_recommended.index
+
+
+cols = list(df_recommended.columns)
+cols = [cols[-1]] + cols[:-1]  
+df_recommended = df_recommended[cols]
+
+###########################################################################################################
+# Fastest at you
+
+fastest_delivery_df = df_valid[df_valid['delivery_time'] <= 3.0]
+
+fastest_delivery_df = fastest_delivery_df.sort_values(by=['delivery_time', 'price'])
+fastest_delivery_df.reset_index(drop=True, inplace=True)
+
+fastest_delivery_df.drop(columns='id', inplace=True)
+
+fastest_delivery_df.reset_index(drop=True, inplace=True)
+fastest_delivery_df.index += 1
+
+fastest_delivery_df['id'] = fastest_delivery_df.index
+
+cols = list(fastest_delivery_df.columns)
+cols = [cols[-1]] + cols[:-1]  
+fastest_delivery_df = fastest_delivery_df[cols]
+
+###########################################################################################################
+# Best deal $
+
+df_best_deal = filtered_df.sort_values(by=['model', 'price', 'delivery_time'])
+df_best_deal.reset_index(drop=True, inplace=True)
+
+df_best_deal.drop(columns='id', inplace=True)
+
+df_best_deal.reset_index(drop=True, inplace=True)
+df_best_deal.index += 1
+
+
+df_best_deal['id'] = df_best_deal.index
+cols = list(df_best_deal.columns)
+cols = [cols[-1]] + cols[:-1]
+df_best_deal = df_best_deal[cols]
+
+
+###########################################################################################################
 from functions import sidebar
 
 def sidebar(df):
